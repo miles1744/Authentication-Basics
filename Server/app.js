eaa1aee524fb11925ed14f5fac153c78c2c6117f
Miles1744 <mithomtab@gmail.com>
2025-05-11 12:40:16 -0400

    New

--- Server/app.js
+++ Server/app.js
@@ -40,7 +40,6 @@
     }
   });
   
-
   
   
   passport.use(
@@ -49,18 +48,19 @@
         const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
         const user = rows[0];
   
-        if (!user) {
-          return done(null, false, { message: "Incorrect username" });
-        }
+        if (!user) return done(null, false, { message: "Incorrect username" });
+  
         if (user.password !== password) {
           return done(null, false, { message: "Incorrect password" });
         }
-        return done(null, user);
-      } catch(err) {
+  
+        return done(null, user);  // 👈 Must return a full user with `id`
+      } catch (err) {
         return done(err);
       }
     })
   );
+  
 
   passport.serializeUser((user, done) => {
     done(null, user.id);

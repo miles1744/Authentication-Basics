a0405514674be2ff6b34c6412b6bba4573a204c5
Miles1744 <mithomtab@gmail.com>
2025-05-11 12:17:00 -0400

    New

--- Server/app.js
+++ Server/app.js
@@ -1,25 +1,25 @@
-const path = require("node:path");
-const { Pool } = require("pg");
-const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require('passport-local').Strategy;
+  const path = require("node:path");
+  const { Pool } = require("pg");
+  const express = require("express");
+  const session = require("express-session");
+  const passport = require("passport");
+  const LocalStrategy = require('passport-local').Strategy;
 
-const pool = new Pool({
+  const pool = new Pool({
 
-});
+  });
 
-const app = express();
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+  const app = express();
+  app.set("views", path.join(__dirname, "views"));
+  app.set("view engine", "ejs");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
-app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
+  app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+  app.use(passport.session());
+  app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
-app.get("/sign-up", (req, res) => res.render("sign-up-form"))
-app.post("/sign-up", async (req, res, next) => {
+  app.get("/", (req, res) => res.render("index"));
+  app.get("/sign-up", (req, res) => res.render("sign-up-form"))
+  app.post("/sign-up", async (req, res, next) => {
     try {
       await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
         req.body.username,
@@ -65,6 +65,13 @@ app.post("/sign-up", async (req, res, next) => {
     }
   });
   
+  app.post(
+    "/log-in",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/"
+    })
+  );
   
 
-app.listen(3000, () => console.log("app listening on port 3000!"));
+  app.listen(3000, () => console.log("app listening on port 3000!"));
